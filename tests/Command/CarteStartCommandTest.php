bb7c5b4001cb2c1fa3940a39285dee853c0f67b9
hamdi <garfi.hamdi@gmail.com>
2021-03-16 03:22:36 +0100

    create tests for command

--- tests/Command/CarteStartCommandTest.php
+++ tests/Command/CarteStartCommandTest.php
@@ -3,15 +3,31 @@
 namespace App\Tests\Command;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use App\Command\CarteStartCommand ;
 
 class CarteStartCommandTest extends KernelTestCase
 {
-    public function testSomething(): void
+    public function testExecute()
     {
         $kernel = self::bootKernel();
+        $application = new Application($kernel);
 
-       // $this->assertSame('test', $kernel->getEnvironment());
-        //$routerService = self::$container->get('router');
-        //$myCustomService = self::$container->get(CustomService::class);
+        $application->add(new CarteStartCommand());
+
+        $command = $application->find('carte:generate');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'  => $command->getName(),
+
+            // pass arguments to the helper
+            'nbrCards' =>10,
+ 
+        ));
+
+        // the output of the command in the console
+        $output = $commandTester->getDisplay();
+        $this->assertContains('nbrCards: 10', $output);
     }
 }
